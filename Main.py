d0a66f4681aed889c1ef4ac4458dab84f0f6cf82
arjuninprocess-create <arjuninprocess@gmail.com>
2026-09-10 12:49:53 +0200

    folder and employees created

--- Main.py
+++ Main.py
@@ -1,2 +1,22 @@
+
+from inside_menu.employees import employees_menu
+
+
+
 print("GYM - Tracker")
+
 input("press any key to get into the GYM tracker:")
+
+print("=" * 35)
+print("        MENU OF GYM _TRACKER            ")
+print("=" * 35)
+print("choose the number you want to go to:")
+
+def menu():
+    print("| 1.      MEMBERSHIP               | ")
+    print("| 2.      EMPLOYEES                | ")
+    print("| 3.      EQUIPMENTS               | ")
+    print("| 4.      SERVICES                 | ")
+    print("| 5.      EXIT                     | ")
+
+employees_menu()
---
+++ inside_menu/employees.py
@@ -0,0 +1,14 @@
+def employees_menu():
+    choice = input("|1.    Manager            |"
+                   "|2.    Trainer's          |"
+                   "|3.    Cleaner's          |"
+                   "|4.    Massage Therapist  |")
+    if choice == "1" or choice == "Manager" or choice == "manager":
+        print("="* 20)
+        print("|     Manager's   |")
+        print("="* 20)
+        print("|1.  Jhony watson |"
+              "|2.  Shima Shibu  |"
+              "|3.  Cristiano R  |")
+        print("="* 20)
+    
